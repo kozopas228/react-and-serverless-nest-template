2257707f88688405323f790fbb81c0cc5731efa3
Andrii <60845315+kozopas228@users.noreply.github.com>
2024-11-19 13:43:36 +0200

    add .gitkeep files to unused folders, add text to about page

--- frontend/src/pages/about/AboutPage.tsx
+++ frontend/src/pages/about/AboutPage.tsx
@@ -2,9 +2,101 @@ import React from 'react';
 
 const AboutPage = () => {
     return (
-        <>
-            About Page
-        </>
+        <div>
+            <h2 className='mb-4 text-3xl font-extrabold'>About</h2>
+            <p className='mb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consequatur explicabo magnam nobis odio quibusdam recusandae
+                reiciendis sapiente tempora vel velit. Animi commodi, cum
+                deleniti, illo ipsum iure iusto maiores, obcaecati quam qui sed
+                ut vero voluptas. Aperiam laborum praesentium quidem repellat?
+                Nisi nobis omnis recusandae reiciendis. Aperiam doloribus
+                dolorum est eum ex expedita impedit in, laudantium maxime nisi
+                nostrum quibusdam, repudiandae tempora. Accusamus accusantium
+                aliquid asperiores aspernatur beatae consequuntur cupiditate
+                debitis dolores eaque eligendi et excepturi explicabo in ipsa
+                molestiae nam nostrum omnis pariatur placeat quas ratione sit,
+                soluta ut vero vitae! Alias animi architecto aspernatur
+                assumenda aut culpa cumque cupiditate deserunt dolor doloremque,
+                dolores dolorum explicabo fuga harum illo impedit ipsam iste
+                itaque labore minus nam necessitatibus nesciunt nobis nostrum
+                omnis possimus provident qui quia quibusdam repellendus sequi
+                sunt suscipit velit? Asperiores consectetur consequatur eaque
+                esse impedit magni maxime nemo omnis, quo sint soluta vel
+                veniam, veritatis voluptas, voluptate. Aliquam blanditiis dolor
+                eveniet, illum omnis voluptatibus! Aut dicta ex laudantium
+                magnam molestiae, nostrum numquam optio porro quae repudiandae
+                sequi sunt velit vero! Alias aliquam aperiam at commodi
+                cupiditate delectus dicta dolorem dolores earum error, est,
+                excepturi explicabo harum impedit in incidunt itaque labore
+                laudantium minus officia omnis perferendis provident quis rem
+                repudiandae ut vel voluptates. Beatae consequuntur doloribus
+                facilis tenetur vero. A ad aliquam commodi consequatur debitis
+                delectus dicta dolor, dolorum ducimus est expedita fugit id
+                inventore iste maxime nesciunt nihil nulla praesentium quis quod
+                repellendus sapiente sequi totam unde velit. Ad aspernatur, at
+                beatae blanditiis, deserunt dolore doloremque eos eum laborum
+                laudantium minus molestias mollitia nulla placeat quasi
+                repellat, reprehenderit repudiandae tempore temporibus ut.
+                Accusantium aperiam doloribus maiores quibusdam, ratione
+                repellat sint? Consequatur, earum et magnam molestias odio omnis
+                praesentium quasi sapiente vitae voluptatum. Beatae, commodi
+                culpa cupiditate dolor incidunt ipsa molestiae molestias nisi
+                quo, repellat vero voluptatum!
+            </p>
+
+            <p className='mb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Incidunt maxime nam odio porro praesentium quo repellat sed
+                vitae? Ab accusamus aperiam assumenda consequatur eum eveniet
+                explicabo facere fugiat impedit labore magni minus natus nulla
+                omnis perferendis possimus recusandae sequi similique,
+                temporibus voluptate! Assumenda corporis culpa dignissimos
+                dolorem error esse eveniet fuga fugit hic illo minima minus
+                mollitia necessitatibus nisi nostrum omnis optio pariatur
+                perferendis qui, quia quibusdam reiciendis repellendus. Alias
+                autem consectetur consequatur, culpa error iure libero maxime
+                minus modi, nemo nisi ratione saepe sapiente tempora voluptatem.
+                Earum ex laborum necessitatibus nulla officia perferendis, qui.
+                Beatae dicta exercitationem explicabo reiciendis.
+            </p>
+
+            <p className='mb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aspernatur aut cupiditate dolores esse est impedit in iste,
+                iusto libero magnam nesciunt non provident sunt! Alias aperiam
+                consequatur deleniti eveniet fugiat illo laborum, nemo nobis non
+                numquam, odit, omnis quae sequi. Ducimus eius perspiciatis
+                recusandae rem tempora velit, voluptas! Aperiam blanditiis
+                dolore ipsa maiores nobis obcaecati quisquam sapiente soluta
+                tenetur. A accusantium aspernatur assumenda atque beatae
+                blanditiis consequatur culpa debitis dolorum eaque error fugiat
+                illum ipsa iste labore laboriosam laudantium mollitia natus
+                numquam, odit officiis perspiciatis quaerat quasi quia quis quod
+                repellendus rerum saepe sapiente similique velit voluptatem
+                voluptates voluptatum! Aliquid asperiores eligendi incidunt
+                natus nulla. Consequuntur eius fugit id laboriosam magnam natus
+                nihil nobis perspiciatis, quam quo sequi tenetur veritatis?
+                Adipisci doloribus eius explicabo laboriosam natus quo
+                recusandae sunt. Beatae distinctio facilis inventore obcaecati
+                officia recusandae rem repellat reprehenderit repudiandae
+                temporibus tenetur, voluptate. Adipisci, blanditiis commodi
+                consequuntur dolorem eaque exercitationem explicabo ipsam iusto
+                libero maxime minima, molestiae mollitia nobis, omnis sit
+                tempore tenetur velit? Corporis illum odit quae similique
+                soluta. Eum ipsum mollitia, nam numquam officiis quia quibusdam
+                quis, rerum sapiente sequi suscipit, veritatis voluptas
+                voluptatem. Accusamus delectus eum eveniet harum, libero optio
+                quasi sed, similique tempore unde voluptas voluptatem! Animi aut
+                consectetur cupiditate dicta dolores ducimus enim expedita
+                minima omnis quam qui, quia quidem quod rerum sapiente tempora
+                ullam. Accusantium distinctio dolorem dolorum obcaecati possimus
+                quibusdam rem? Architecto aspernatur blanditiis delectus
+                dignissimos doloremque eos expedita fugit iure libero minus,
+                nemo nostrum numquam possimus quas quo ratione tenetur vitae
+                voluptate!
+            </p>
+        </div>
     );
 };
 
