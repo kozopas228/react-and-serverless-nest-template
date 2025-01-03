1583231a3f26bda58422ffee854b59b4698caa5a
Andrii <60845315+kozopas228@users.noreply.github.com>
2025-01-03 12:19:57 +0200

    extend .gitignore, add tags to frontend cloudformation.yml, add vite-svgr,

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [svgr(), react()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
